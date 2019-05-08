a7c38c794fceab11e962f27538fd487311e01e60
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-08 22:12:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(integer)
+  if integer == 2
+    return true
+  elsif integer == 0 || integer == 1
+    return false
+  elsif integer % 2 == 0
+    return false
+  else 2.upto(Math.floor(Math.sqrt(integer))) do |x|
+    if integer % x == 0
+      return false
+    else
+      return true
+    end
+  end
+end
+
